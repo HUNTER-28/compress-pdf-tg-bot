fd3988308965b8786db8679b5ff3f8b231e9d495
HUNTER-28 <65951008+HUNTER-28@users.noreply.github.com>
2022-02-28 14:06:57 +0200

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,8 +1,9 @@
 from presets import Presets
 from pyrogram.types import Message
 from pyrogram import Client, filters
+from support.markups import start_button
 
 
 @Client.on_message(filters.private & filters.command('start'))
 async def start_bot(c, m: Message):
-    await m.reply_text(Presets.WELCOME_TXT.format(m.from_user.first_name))
+    await m.reply_text(Presets.WELCOME_TXT.format(m.from_user.first_name), reply_markup=start_button)
