3bcc4397fa6e09ca6d17d0b511e7d3a1eaa686a0
HUNTER-28 <65951008+HUNTER-28@users.noreply.github.com>
2022-02-28 13:19:46 +0200

    Update compress.py

--- plugins/compress.py
+++ plugins/compress.py
@@ -12,7 +12,7 @@ from support.display_progress import progress_for_pyrogram
 
 @Client.on_message(filters.private & filters.document)
 async def compress_pdf(c, m: Message):
-    msg = await m.reply_text(Presets.WAIT_MESSAGE, reply_to_message_id=m.message_id)
+    msg = await m.reply_sticker("AnimatedSticker.tgs", reply_to_message_id=m.message_id)
     if not str(m.document.file_name).lower().endswith('.pdf'):
         await msg.edit(Presets.INVALID_FORMAT, reply_markup=close_button)
         return
@@ -27,25 +27,8 @@ async def compress_pdf(c, m: Message):
             except Exception:
                 pass
     #
-    await asyncio.sleep(2)
-    await msg.edit(Presets.DOWNLOAD_MSG)
-    current_time = time.time()
-    await m.download(
-        file_name=dl_location,
-        progress=progress_for_pyrogram,
-        progress_args=(
-            Presets.DOWNLOAD_MSG,
-            msg,
-            current_time
-        )
-    )
-    #
-    await asyncio.sleep(1)
-    await msg.edit(Presets.FINISHED_DL)
-    await asyncio.sleep(2)
-    await msg.edit(Presets.START_COMPRESSING)
-    await asyncio.sleep(2)
-    #
+    await m.download(file_name=dl_location)
+    
     # Let's find out the initial document size
     size_path = await get_size(dl_location)
     initial_size = size_path[0]
@@ -65,26 +48,14 @@ async def compress_pdf(c, m: Message):
         doc.Save(size_path[1], SDFDoc.e_linearized)
         doc.Close()
     except Exception:
-        await msg.edit(Presets.JOB_ERROR, reply_markup=close_button)
+        await msg.delete()
+        await m.reply_text(Presets.JOB_ERROR, reply_markup=close_button)
         return
-    #
-    # Let's find out the compressed document file size
-    size_path = await get_size(dl_location)
-    compressed_size = size_path[0]
-    #
-    await asyncio.sleep(2)
-    message = await msg.edit(Presets.UPLOAD_MSG)
-    current_time = time.time()
     #
     await m.reply_document(
         document=size_path[1],
         reply_to_message_id=m.message_id,
-        caption=m.caption if m.caption else '',
-        progress=progress_for_pyrogram,
-        progress_args=(
-            Presets.UPLOAD_MSG,
-            message,
-            current_time
+        caption=m.caption if m.caption else ''
         )
 
     )
@@ -94,7 +65,8 @@ async def compress_pdf(c, m: Message):
     except Exception:
         pass
     #
-    await msg.edit(Presets.FINISHED_JOB.format(initial_size, compressed_size),
+    await msg.delete()
+    await m.reply_text(Presets.FINISHED_JOB,
                    disable_web_page_preview=True,
                    reply_markup=close_button
                    )
